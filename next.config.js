704ba259393986e714a9ccfd5f275740225af853
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-02-12 18:17:22 +0100

    Change app to pwa and next 14 with app router

--- next.config.js
+++ next.config.js
@@ -1,26 +1,29 @@
 /** @type {import('next').NextConfig} */
 
-const { i18n } = require('./next-i18next.config');
-
 const nextConfig = {
-  reactStrictMode: true,
-  i18n,
-  experimental: {
-    esmExternals: true
-  },
-  env: {
-    API_KEY: process.env.REACT_APP_API_KEY,
-    DB_ID: process.env.REACT_APP_DB_ID
-  },
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true
-  }
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    DB_ID: process.env.NEXT_PUBLIC_DB_ID,
+  },
 };
 
-module.exports = nextConfig;
+// Configuration object tells the next-pwa plugin
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig);
