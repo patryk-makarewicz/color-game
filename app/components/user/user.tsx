3e1a47230af3a8f9571166fa81f3f0ad69e97858
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-02-26 09:48:33 +0100

    Fix issue with update start button

--- app/components/user/user.tsx
+++ app/components/user/user.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link';
 
+import { useEffect, useState } from 'react';
+
 import { Button } from '@/components/button';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { useTranslation } from '@/i18n/client';
@@ -9,10 +11,15 @@ import { useTranslation } from '@/i18n/client';
 export const User = ({ lng }: { lng: string }) => {
   const { t } = useTranslation(lng);
   const [inputValue, setInputValue] = useLocalStorage('color-game-user', '');
+  const [disabledButton, setDisabledButton] = useState(true);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
+
+  useEffect(() => {
+    setDisabledButton(inputValue.length < 3);
+  }, [inputValue]);
 
   return (
     <div className="flex w-full flex-col items-center gap-6">
@@ -26,7 +33,7 @@ export const User = ({ lng }: { lng: string }) => {
         maxLength={20}
       />
       <Link href={`/${lng}/game`}>
-        <Button disabled={inputValue.length < 3}>{t('components.button.start')}</Button>
+        <Button disabled={disabledButton}>{t('components.button.start')}</Button>
       </Link>
     </div>
   );
