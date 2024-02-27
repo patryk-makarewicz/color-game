e5af07ca7f092744f3a65c27ea74a832106a67c8
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-02-27 22:48:51 +0100

    Create Services

---
+++ app/services/index.ts
@@ -0,0 +1,3 @@
+export * from './request';
+
+export * as QuestionsAPI from './questions/questions.api';
---
+++ app/services/questions/questions.api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+import { useAPImocks } from '../config';
+import { getQuestionsListMock } from './questions.mock';
+import { QuestionsDTO } from './questions.model';
+
+export const getQuestionsList = () =>
+  !useAPImocks ? getQuestionsListMock() : axios.get<QuestionsDTO>('/api/questions').then(({ data }) => data);
