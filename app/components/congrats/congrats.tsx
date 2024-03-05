457c864b10bcd2a04a4e94eeb07478a8c1e23854
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-03-05 22:29:00 +0100

    Rename component

--- app/components/congrats/congrats.tsx
+++ app/components/congrats/congrats.tsx
@@ -3,9 +3,13 @@
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { useTranslation } from '@/i18n/client';
 
-export const Greetings = ({ lng }: { lng: string }) => {
+export const Congrats = ({ lng }: { lng: string }) => {
   const { t } = useTranslation(lng);
   const [userName] = useLocalStorage('color-game-user', '');
 
-  return <p className="text-lg">{t('page.game.greetings', { user: userName })}</p>;
+  return (
+    <p className="text-lg">
+      <span className="font-bold text-appPrimary">{userName}</span> {t('page.game.congrats')}
+    </p>
+  );
 };
