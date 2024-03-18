43c537fefb39cb71f727bea3cf6b2054bfe17400
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-03-18 22:10:17 +0100

    Add logo to title component

--- app/components/typography/title/title.tsx
+++ app/components/typography/title/title.tsx
@@ -1,7 +1,12 @@
+import { Logo } from '@/components/logo/logo';
 import { useTranslation } from '@/i18n';
 
 export const Title = async ({ lng }: { lng: string }) => {
   const { t } = await useTranslation(lng);
 
-  return <h1 className="text-2xl font-bold text-appPrimary">{t('app')}</h1>;
+  return (
+    <h1 className="flex items-center gap-3 text-2xl font-bold text-appPrimary">
+      <Logo /> {t('app')}
+    </h1>
+  );
 };
