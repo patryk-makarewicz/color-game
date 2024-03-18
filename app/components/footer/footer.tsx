7d920fcf946834e38f733cafee4d398103239f74
Patryk Makarewicz <64835775+patryk-makarewicz@users.noreply.github.com>
2024-03-18 21:31:20 +0100

    [Fix] Layout (#9)
    * Add footer
    
    * Change layout
    
    * Adjust layout

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -6,9 +6,9 @@ export const Footer = async ({ lng }: { lng: string }) => {
   const year = date.getFullYear();
 
   return (
-    <footer className="relative border-t border-appGrayLight bg-white">
-      <div className="mx-auto flex max-w-screen-2xl items-center justify-center p-2.5">
-        <p className="text-center text-sm" data-testid="copyright">
+    <footer className="relative bg-white">
+      <div className="mx-auto flex max-w-md items-center justify-center border-t border-appGrayLight p-2.5">
+        <p className="text-center text-xs text-appGray" data-testid="copyright">
           {t('footer.copyright', { year: year })}
         </p>
       </div>
