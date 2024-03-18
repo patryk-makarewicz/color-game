758b42ca65951c89598ee3fc5f633ef075f7385e
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-03-18 21:26:25 +0100

    Adjust layout

--- app/layouts/gameLayout.tsx
+++ app/layouts/gameLayout.tsx
@@ -1,5 +1,5 @@
 export const GameLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="m-auto flex min-h-[calc(100vh-61px)] w-fit max-w-md flex-col items-center justify-center gap-6 p-2">
+  <div className="m-auto flex min-h-[calc(100dvh-37px)]  w-fit max-w-md flex-col items-center justify-center gap-6 p-2">
     {children}
   </div>
 );
