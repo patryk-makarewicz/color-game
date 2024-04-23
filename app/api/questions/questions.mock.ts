4610bfb6bf0be11883dc2b770826517be7605bbd
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-04-23 22:03:16 +0200

    Change questions

--- app/api/questions/questions.mock.ts
+++ app/api/questions/questions.mock.ts
@@ -5,45 +5,45 @@ export const getQuestionsListMock = () => {
   return request<QuestionsDTO>([
     {
       id: '1',
-      question: 'Który z kolorów to biały?',
-      options: ['red', 'white', 'yellow', 'green'],
-      goodAnswer: 'white'
+      question: 'Który z kolorów to white?',
+      options: ['czerwony', 'biały', 'żółty', 'zielony'],
+      goodAnswer: 'biały'
     },
     {
       id: '2',
-      question: 'Który z kolorów to czarny?',
-      options: ['black', 'blue', 'purple', 'red'],
-      goodAnswer: 'black'
+      question: 'Który z kolorów to black?',
+      options: ['czarny', 'niebieski', 'fioletowy', 'czerwony'],
+      goodAnswer: 'czarny'
     },
     {
       id: '3',
-      question: 'Który z kolorów to niebieski?',
-      options: ['yellow', 'green', 'red', 'blue'],
-      goodAnswer: 'blue'
+      question: 'Który z kolorów to blue?',
+      options: ['żółty', 'zielony', 'czerwony', 'niebieski'],
+      goodAnswer: 'niebieski'
     },
     {
       id: '4',
-      question: 'Który z kolorów to czerwony?',
-      options: ['white', 'black', 'red', 'purple'],
-      goodAnswer: 'red'
+      question: 'Który z kolorów to red?',
+      options: ['biały', 'czarny', 'czerwony', 'fioletowy'],
+      goodAnswer: 'czerwony'
     },
     {
       id: '5',
-      question: 'Który z kolorów to żółty?',
-      options: ['yellow', 'green', 'blue', 'black'],
-      goodAnswer: 'yellow'
+      question: 'Który z kolorów to yellow?',
+      options: ['żółty', 'zielony', 'niebieski', 'czarny'],
+      goodAnswer: 'żółty'
     },
     {
       id: '6',
-      question: 'Który z kolorów to zielony?',
-      options: ['blue', 'green', 'purple', 'white'],
-      goodAnswer: 'green'
+      question: 'Który z kolorów to green?',
+      options: ['niebieski', 'zielony', 'fioletowy', 'biały'],
+      goodAnswer: 'zielony'
     },
     {
       id: '7',
-      question: 'Który z kolorów to fioletowy?',
-      options: ['purple', 'red', 'yellow', 'black'],
-      goodAnswer: 'purple'
+      question: 'Który z kolorów to <b>purple</b>?',
+      options: ['fioletowy', 'czerwony', 'żółty', 'czarny'],
+      goodAnswer: 'fioletowy'
     }
   ]);
 };
--- app/helpers/colorsClasses.ts
+++ app/helpers/colorsClasses.ts
@@ -1,9 +1,9 @@
 export const colorClasses: { [key: string]: string } = {
-  red: 'bg-red-500',
-  white: 'bg-white',
-  yellow: 'bg-yellow-500',
-  green: 'bg-green-500',
-  black: 'bg-black',
-  blue: 'bg-blue-500',
-  purple: 'bg-purple-500'
+  czerwony: 'bg-red-500',
+  biały: 'bg-white',
+  żółty: 'bg-yellow-500',
+  zielony: 'bg-green-500',
+  czarny: 'bg-black',
+  niebieski: 'bg-blue-500',
+  fioletowy: 'bg-purple-500'
 };
